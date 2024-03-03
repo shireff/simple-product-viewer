95f2e1c6e24e7424483fc3dea455562a3773980d
Shireff Nady <137368868+ShireffNady@users.noreply.github.com>
2024-03-03 22:11:36 +0200

    edit responsive

--- app/_components/btns/Follow.jsx
+++ app/_components/btns/Follow.jsx
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import Image from 'next/image'
-// import ".//follow.css"
-// function Follow(props) {
-//   const {img, h2, p} = props
-//   return (
-//     <div className="flex flex-row items-center gap-[2px] sm:w-[100px] sm:h-[59px] lg:w-[108px] h-[59px] rounded-[18px] p-[5px] fo-main">
-//       <Image
-//       className="fo-icon"
-//       alt="tip"
-//       src={img}
-//       width={24}
-//       height={24}
-//        />
-//        <div>
-//         <h2 className="text-[14px] font-bold leading-[19.1px]">{h2}</h2>
-//         <p className="text-[12px] font-normal leading-[16.37px] pp">{p}</p>
-//        </div>
-//     </div>
-//   )
-// }
-
-// export default Follow
-
 
 import React from 'react'
 import Image from 'next/image'
--- app/_components/hero/Hero.jsx
+++ app/_components/hero/Hero.jsx
@@ -1,49 +1,3 @@
-// import React from "react";
-// import Image from "next/image";
-// import ".//hero.css"
-// import Products from "../products/Products";
-// function Hero() {
-//   return (
-//     <div className="desc-side flex flex-col p-[16px] lg:w-[780px] sm:w-[343px] h-[931px] rounded-[21px]">
-//       <div className="flex items-center justify-between sm:w-[311px]">
-//         <div className="flex gap-[10px] text-center text-[14px] font-bold leading-[19.1px]">
-//           <button className="btn1 lg:w-[108px] sm:w-[97px] h-[35px] font-bold rounded-[14px] p-[8px]">Products</button>
-//           <button className="btn2 lg:w-[108px] sm:w-[97px] h-[35px] font-normal rounded-[14px] p-[8px]">Articles</button>
-//           <button className="btn2 lg:w-[108px] sm:w-[97px] h-[35px] font-normal rounded-[14px] p-[8px]">Reviews</button>
-//         </div>
-//         <button
-//           className="flex items-center gap-[4px] rounded-[10px] lg:w-[125px] h-[40px] px-5 py-2.5 text-sm font-medium text-white sm:hidden btn"
-//           href="#"
-//         >
-//           <Image
-//             className="rounded-full icon-img"
-//             src={"/img/add-circle.svg"}
-//             width={18}
-//             height={18}
-//             alt="search-icon"
-//           />
-//           Add Review
-//         </button>
-//       </div>
-//       <div className="flex items-center gap-[8px]">
-//         <h2 className="text-[32px] sm:text-[24px] font-extrabold leading-[43.56px]">
-//           Products
-//         </h2>
-//         <span className="text-[14px] font-normal leading-[19.1px] span">(12)</span>
-//       </div>
-//       <div className="flex flex-col gap-[24px]">
-//         <Products cls="sub-product-img" img="/img/product1.svg" subImg="/img/hh.svg"/>
-//         <Products cls="sub-product-img" img="/img/sub-product-img2.svg" subImg="/img/hh.svg"/>
-//         <Products cls="sub-product-img1" img="/img/sub-product-img3.svg" subImg="/img/hh.svg"/>
-//         <Products cls="sub-product-img" img="/img/sub-product-img4.svg" subImg="/img/hh.svg"/>
-//         <Products cls="sub-product-img" img="/img/sub-product-img5.svg" subImg="/img/hh.svg"/>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Hero;
-
 
 import React from "react";
 import Image from "next/image";
@@ -53,14 +7,14 @@ import Products from "../products/Products";
 function Hero() {
   return (
     <div className="desc-side flex flex-col p-[16px] lg:w-[780px] sm:w-full h-[auto] rounded-[21px]">
-      <div className="flex items-center justify-between sm:flex-col sm:w-full">
+      <div className="flex items-center justify-between sm:w-full">
         <div className="flex items-center justify-between gap-[10px] text-center text-[14px] font-bold leading-[19.1px]">
           <button className="btn1 lg:w-[108px] sm:w-[97px] h-[35px] font-bold rounded-[14px] p-[8px]">Products</button>
           <button className="btn2 lg:w-[108px] sm:w-[97px] h-[35px] font-normal rounded-[14px] p-[8px]">Articles</button>
           <button className="btn2 lg:w-[108px] sm:w-[97px] h-[35px] font-normal rounded-[14px] p-[8px]">Reviews</button>
         </div>
         <button
-          className="flex items-center gap-[4px] rounded-[10px] lg:w-[125px] h-[40px] px-5 py-2.5 text-sm font-medium text-white sm:w-full sm:mt-4 btn"
+          className="Add-Review flex items-center gap-[4px] rounded-[10px] lg:w-[125px] h-[40px] px-5 py-2.5 text-sm font-medium text-white sm:w-full sm:mt-4 btn"
           href="#"
         >
           <Image
